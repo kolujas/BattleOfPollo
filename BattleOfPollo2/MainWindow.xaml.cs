574e37d39ff624d657c5bc05a49912c1cbf4452f
kolujas <fernandodeibe@gmail.com>
2024-08-12 16:07:02 -0300

    Agregada la bonificación de defensa y su respectivo calculo

--- BattleOfPollo2/MainWindow.xaml.cs
+++ BattleOfPollo2/MainWindow.xaml.cs
@@ -24,26 +24,47 @@ namespace BattleOfPollo2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Obtengo el valor del textBox dmg
+            // VARIABLES
+
+            // Obtengo el valor del TextBox dmg
             string dmgText = dmgTextBox.Text;
             double dmgDouble;
 
-            // Obtengo el valor del textBox def
+            // Obtengo el valor del TextBox def
             string defText = dmgTextBox.Text;
             double defDouble;
 
-            // Intento parsear ambos valores
+            // Obtengo el valor del TextBox Bonus Damage 
+            string dmgBonus = dmgBonusTextBox.Text;
+            double dmgBonusDouble;
+
+            // Obtengo el valor del Textbos Bonus defense
+            string defBonus = defBonusTextBox.Text;
+            double defBonusDouble;
+
+
+            // COMPROBACIÓN DEL PARSEO
+
+
+            // Intento parsear los valores
             bool parseoDmg = double.TryParse(dmgText, out dmgDouble);
             bool parseoDef = double.TryParse(defText, out defDouble);
+            bool parseoBonusDmg = double.TryParse(dmgBonus, out dmgBonusDouble);
+            bool parseoBonusDef = double.TryParse(defBonus, out defBonusDouble);
 
-            
-            if (parseoDmg && parseoDef) // Si se parsea correctamente
+            double resultado; // Resultado Final
+            double resultRounded; // Redondeo del resultado final
+
+            #region
+
+            if (parseoDmg && parseoDef && parseoBonusDmg && parseoBonusDef) // Si se parsea correctamente
             {
                 // Se hace el calculo del daño recibido
-                double resultado = (dmgDouble) - (defDouble * 0.6);
+                resultado = (dmgDouble) - (defDouble * 0.6);
+                resultado += (dmgBonusDouble - defBonusDouble);
 
                 // Redondeamos el resultado
-                double resultRounded = Math.Round(resultado);
+                resultRounded = Math.Round(resultado);
 
                 if (resultRounded < 0) // Si el valor da menos de 0 muestro en pantalla 0 (como daño minimo recibido)
                 {
@@ -53,13 +74,13 @@ namespace BattleOfPollo2
                 {
                     lblRes.Content = "Daño recibido: " + resultRounded.ToString();
                 }
-            } 
+            }
             else // Sino se parsea correctamente muestro en pantalla mensajes de errores
             {
                 lblRes.Content = "¡ERROR!";
                 MessageBox.Show("¡Error! Ingresa formato numérico PELOTUDO");
             }
-
+            #endregion
         }
     }
 }
